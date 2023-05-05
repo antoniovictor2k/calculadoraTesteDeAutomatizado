79fcea55bda6c99a2fd6b642bc65d8c0ef08ac31
Antonio <avps2@aluno.ifal.edu.br>
2023-05-05 11:08:14 -0300

    add

--- Calculadora Teste/cypress/e2e/calculadora.cy.js
+++ Calculadora Teste/cypress/e2e/calculadora.cy.js
@@ -3,7 +3,7 @@
 describe('Testando a Função Soma', ()=>{
 
 beforeEach(()=>{
-    cy.visit('http://localhost/Calculadora')
+    cy.visit('http://127.0.0.1:5500/index.html')
 })
 
 it('Testando 10+3',()=>{
@@ -11,7 +11,7 @@ cy.get('#numero1').click()
 cy.get('#numero1').type('10')
 cy.get('#numero2').click()
 cy.get('#numero2').type('3')
-cy.get('[onclick="soma()"]').click()
+cy.get('#soma').click()
 cy.get('#resultadoDoCalculo').should("have.text", '13')
 
 } )
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # calculadoraTesteDeAutomatizado
  teste Automatizado de uma Calculadora
+
+
+ # Instalação Do Cypress
+
+ # 1º abri o terminal e executa os demais comados no cmd ou node. 
+
+* Localizar a pasta do arquivo.
+
+ # 2º Executar os comandos
+
+ * npm init -y
+* npm install cypress
+
+ # 3º Abrir o cypress e configurar
+
+* .\node_modules\.bin\cypress open
+
+ # 4º
+
+ # 5º
